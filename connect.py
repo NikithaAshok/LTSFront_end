01fb1ba124432acb9487df34fa870b5ad325bb4a
NikithaAshok <94978888+NikithaAshok@users.noreply.github.com>
2023-04-21 23:15:34 +0530

    images

--- connect.py
+++ connect.py
@@ -5,15 +5,14 @@ from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig
 import nltk 
 nltk.download('punkt') 
 from nltk.corpus import stopwords 
-from nltk.tokenize import sent_tokenize,word_tokenize 
+from nltk.tokenize import word_tokenize 
 from nltk.stem import PorterStemmer 
-import string 
 import pickle
+from PIL import Image
 
 # Step 1: Load the model from the saved pickle file
 with open("C:/Users/Harsha Ashok/Downloads/bart_model.pkl", "rb") as f:
     model = pickle.load(f)
-
 
 #function for chunking
 def split_text(text):
@@ -32,8 +31,6 @@ def split_text(text):
         
     chunks.append(current_chunk)
     return chunks
-
-
 
 # Home page
 def home():
@@ -59,11 +56,35 @@ def home():
         st.write("Output:")
         st.text_area("", value=final_summary)
        
-
 # About page
 def about():
-    st.header("About")
-    st.write("This is the about page. Here, you can display information about how text summarization works.")
+    st.title("About")
+    st.subheader(":wavy_dash::wavy_dash::wavy_dash::wavy_dash::wavy_dash::wavy_dash::wavy_dash::wavy_dash:")
+    st.header("What is :violet[BriefLegal] :question:")
+    st.subheader("Attorneys, judges, lawyers and others in the justice system are constantly surrounded by large amounts of the legal text, which can be difficult to manage across many cases.")
+    st.subheader(":heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign:")
+    st.subheader("To this day, they face the problem of organizing briefs, judgements and acts.")
+    st.subheader(":heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign:")
+    st.subheader("Our application aims to build a medium where the synergy between law and technology will finally aid the litigators, solicitor or paralegals to smoothen their work system, improve efficiency and nimble their research.")
 
+def contact():
+    st.title('Contact Us! ')
+    st.header('We would love to hear from you! :call_me_hand: :smile:')
+    
+    # Facebook link and logo
+    fb = Image.open('./facebook.png')
+    st.image(fb, caption=None, width=40, use_column_width=25)
+    st.write('[Facebook](https://www.facebook.com/)')
+    
+    # Twitter link and logo
+    twitter = Image.open('./twitter.png')
+    st.image(twitter, caption=None, width=40, use_column_width=25)
+    st.write('[Twitter](https://twitter.com/)')
+    
+    # LinkedIn link and logo
+    linkedin = Image.open('./linkedin.png')
+    st.image(linkedin, caption=None, width=40, use_column_width=25)
+    st.write('[LinkedIn](https://www.linkedin.com/)')
+    
 if __name__ == "__main__":
-    home()
+    about()
